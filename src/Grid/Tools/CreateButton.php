a031b59a878d3e4d31de30742cd9dc1288035bee
Rechik <rechik.development@gmail.com>
2019-11-22 01:23:30 +0200

    Replace btn-success with btn-twitter

--- src/Grid/Tools/CreateButton.php
+++ src/Grid/Tools/CreateButton.php
@@ -37,7 +37,7 @@ class CreateButton extends AbstractTool
         return <<<EOT
 
 <div class="btn-group pull-right grid-create-btn" style="margin-right: 10px">
-    <a href="{$this->grid->getCreateUrl()}" class="btn btn-sm" title="{$new}">
+    <a href="{$this->grid->getCreateUrl()}" class="btn btn-sm btn-twitter" title="{$new}">
         <i class="fa fa-plus"></i><span class="hidden-xs">&nbsp;&nbsp;{$new}</span>
     </a>
 </div>
